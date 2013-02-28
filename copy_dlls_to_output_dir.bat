958327fd54854d088b40aae388e981c37e0da534
Jens Schmer <der.liggers@gmail.com>
2013-02-28 15:32:43 +0100

    fixed batch script for copying the dll dependency to the output directory

--- copy_dlls_to_output_dir.bat
+++ copy_dlls_to_output_dir.bat
@@ -3,8 +3,4 @@ SET PROJECT_DIR=%1
 SET CONFIGURATION=%2
 
 rem bin folder
-copy %PROJECT_DIR%dependencies\FreeImage.dll %PROJECT_DIR%bin\%CONFIGURATION%
-
-rem scenes folder
-copy %PROJECT_DIR%dependencies\FreeImage.dll %PROJECT_DIR%scenes\
-copy %PROJECT_DIR%bin\%CONFIGURATION%\raytracer.exe %PROJECT_DIR%scenes\
+copy %PROJECT_DIR%lib\FreeImage\FreeImage.dll %PROJECT_DIR%bin\%CONFIGURATION%
